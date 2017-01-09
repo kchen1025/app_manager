3f0d2a664bc0e782bc86d0bfceab5052482b0a4e
Kevin Chen <kchen1025@gmail.com>
2017-01-08 20:33:53 -0800

    finish user edit, update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+	before_action :logged_in_user, only: [:index,:edit,:update,:destroy]
+	before_action :correct_user, only: [:edit,:update]
+	before_action :admin_user, only: :destroy
+
+	def index
+		@users = User.all
+	end
+
+
   def new
   	@user = User.new
   end
@@ -16,8 +25,31 @@ class UsersController < ApplicationController
   	else
   		render 'new'
   	end
-
   end
+
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update_attributes(user_params)
+  		#handle successful update
+  		flash[:success] = "Profile updated"
+  		redirect_to @user
+  	else
+  		render 'edit'
+  	end		
+  end
+
+
+  def destroy
+  	User.find(params[:id]).destroy
+  	flash[:success] = "user deleted"
+  	redirect_to users_url
+  end
+
 
   private
 
@@ -25,5 +57,29 @@ class UsersController < ApplicationController
   		#this is the safe way to pass information with a POST request
   		params.require(:user).permit(:name,:email,:password,:password_confirmation)
   	end
+
+  	#before filters
+
+  	#confirms logged in user
+  	def logged_in_user
+  		unless logged_in?
+  			flash[:danger] = "please log in"
+  			redirect_to login_url
+  		end
+  	end
+
+  	def correct_user
+  		@user = User.find(params[:id])
+  		redirect_to(root_url) unless @user == current_user
+  	end
+
+  	def admin_user
+  		
+  		unless current_user.admin?
+  			redirect_to(root_url)
+  		end
+  	end
+
+
 
 end
